01f6098a41f56460fe9042fb4b397f22fe63eebb
77 | rax <116083042+raox77@users.noreply.github.com>
2023-03-19 17:14:15 +0400

    Update Run this after Brave setup.bat

--- src/Postinstall/2-Browser/Run this after Brave setup.bat
+++ src/Postinstall/2-Browser/Run this after Brave setup.bat
@@ -1,8 +1,10 @@
 @echo off
-net stop brave
-sc delete brave
-net stop bravem
-sc delete bravem
-taskkill /f /im BraveUpdate.exe
-rmdir "C:\Program Files (x86)\BraveSoftware\Update" /s /q
-pause
+
+del "C:\Program Files\Mozilla Firefox\maintenanceservice_installer.exe" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\minidump-analyzer.exe" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\pingsender.exe" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\updater.exe" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\crashreporter.ini" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\maintenanceservice.exe" /f /q >nul 2>&1
+del "C:\Program Files\Mozilla Firefox\crashreporter.exe" /f /q >nul 2>&1
+
