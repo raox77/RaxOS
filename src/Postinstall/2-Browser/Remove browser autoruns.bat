0485dd8f30552b7086dbc89f96f5d3e47522d283
77 | rax <116083042+raox77@users.noreply.github.com>
2023-03-19 17:14:04 +0400

    Delete Remove browser autoruns.bat

--- src/Postinstall/2-Browser/Remove browser autoruns.bat
+++
@@ -1,2 +0,0 @@
-Reg.exe delete "HKLM\Software\Microsoft\Active Setup\Installed Components\{8A69D345-D564-463c-AFF1-A69D9E530F96}" /f
-Reg.exe delete "HKLM\Software\Microsoft\Active Setup\Installed Components\{AFE6A462-C574-4B8A-AF43-4CC60DF4563B}" /f
