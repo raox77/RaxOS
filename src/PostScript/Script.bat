6d658a778595e2f75d8215dd07a1d5bba4f5f62d
77 | rax <116083042+raox77@users.noreply.github.com>
2023-03-20 12:34:14 +0400

    Update Script.bat

--- src/PostScript/Script.bat
+++ src/PostScript/Script.bat
@@ -1,35 +1,6 @@
 @echo off 
 Title RaxOS 
 setlocal EnableDelayedExpansion
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
-
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params= %*
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-
 
 Echo "Installing Visual C Runtimes"
 start /b /wait "" "C:\Modules\VisualCAIO\install_all.bat" >nul 2>&1
