4a79e6fa6f71f71be296a3478b0652c25a0fdec5
77 | rax <116083042+raox77@users.noreply.github.com>
2023-07-08 22:19:54 +0400

    Update cleanup.bat

--- src/cleanup.bat
+++ src/cleanup.bat
@@ -1,4 +1,6 @@
 @echo off
 takeown /f "%WinDir%\Modules" /a & icacls "%WinDir%\Modules" /grant Administrators:F
-rd /s /q "%WinDir%\Modules" 
-del "%~f0"&exit 
+rd /s /q "%WinDir%\Modules"
+takeown /f "C:\Modules" /a & icacls "C:\Modules" /grant Administrators:F
+rd /s /q "C:\Modules"
+del "%~f0"&exit
