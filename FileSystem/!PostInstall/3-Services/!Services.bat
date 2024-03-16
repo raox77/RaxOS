079c673d01ffa27441c9553b2555ede6bd80f39c
77 | rax <116083042+raox77@users.noreply.github.com>
2024-03-16 16:50:31 +0400

    Update !Services.bat

--- FileSystem/!PostInstall/3-Services/!Services.bat
+++ FileSystem/!PostInstall/3-Services/!Services.bat
@@ -104,6 +104,7 @@ if %bluetooth_option%==1 (
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BTHPORT" /v "Start" /t REG_DWORD /d "3" /f
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BthMini" /v "Start" /t REG_DWORD /d "3" /f
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\HidBth" /v "Start" /t REG_DWORD /d "3" /f
+    devmanview /enable "Microsoft Radio Device Enumeration Bus" > NUL 2>&1
     cls
     echo Bluetooth has been Enabled
     timeout 2 > nul
@@ -124,6 +125,7 @@ if %bluetooth_option%==1 (
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BTHPORT" /v "Start" /t REG_DWORD /d "4" /f
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BthMini" /v "Start" /t REG_DWORD /d "4" /f
     PowerRun.exe /SW:0 Reg.exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\HidBth" /v "Start" /t REG_DWORD /d "4" /f
+    devmanview /disable "Microsoft Radio Device Enumeration Bus" > NUL 2>&1
     cls
     echo Bluetooth has been Disabled
     timeout 2 > nul
